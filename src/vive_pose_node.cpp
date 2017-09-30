d0c3dd9fa44cbff8198f5107a1086e481969ba9f
Jarvis Schultz <schultzjarvis@gmail.com>
2017-09-29 20:10:35 -0500

    Cleaning up unnecessary files and adding PoseStamped pub

--- src/vive_pose_node.cpp
+++ src/vive_pose_node.cpp
@@ -7,7 +7,10 @@
 #include <linmath.h>
 #include <survive_config.h>
 #include <ros/ros.h>
-#include <tf/transform_broadcaster.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
 
 struct SurviveContext * ctx;
 int quit = 0;
@@ -64,19 +67,44 @@ int main(int argc, char** argv)
       exit( 1 );
     }
 
+
+  // setup ROS stuff:
+  ros::NodeHandle n;
+  tf2_ros::TransformBroadcaster br;
+  ros::Publisher pose_pub = n.advertise<geometry_msgs::PoseStamped>("tracker_pose", 1);
+
   // Put main code here
   // Full pose (Pos[3] and Quat[4]) will be available in objPose[0]
   while(survive_poll(ctx) == 0 && !quit)
     {
       if (PoseLoaded && (timestamp != timestampprev))
         {
-	  static tf::TransformBroadcaster br;
-	  tf::Transform transform;
-	  transform.setOrigin(tf::Vector3(objPose[0].Pos[0], objPose[0].Pos[1], -objPose[0].Pos[2]));
-	  tf::Quaternion q = tf::Quaternion(objPose[0].Rot[0], objPose[0].Rot[1], objPose[0].Rot[2], objPose[0].Rot[3]);
-	  transform.setRotation(q);
-	  br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "lighthouse", "tracker"));
-	  // printf("%d -- %d : (%f, %f, %f)\n", timestampprev, timestamp, objPose[0].Pos[0], objPose[0].Pos[1], objPose[0].Pos[2]);
+	  geometry_msgs::TransformStamped transformStamped;
+	  transformStamped.header.stamp = ros::Time::now();
+	  transformStamped.header.frame_id = "lighthouse";
+	  transformStamped.child_frame_id = "tracker";
+	  transformStamped.transform.translation.x = objPose[0].Pos[0];
+	  transformStamped.transform.translation.y = objPose[0].Pos[1];
+	  transformStamped.transform.translation.z = -objPose[0].Pos[2];
+	  transformStamped.transform.rotation.x = objPose[0].Rot[0];
+	  transformStamped.transform.rotation.y = objPose[0].Rot[1];
+	  transformStamped.transform.rotation.z = objPose[0].Rot[2];
+	  transformStamped.transform.rotation.w = objPose[0].Rot[3];
+	  br.sendTransform(transformStamped);
+
+	  // now we can convert to a PoseStamped and pub:
+	  geometry_msgs::PoseStamped p;
+	  p.header = transformStamped.header;
+	  p.pose.position.x = transformStamped.transform.translation.x;
+	  p.pose.position.y = transformStamped.transform.translation.y;
+	  p.pose.position.z = transformStamped.transform.translation.z;
+	  p.pose.orientation.x = transformStamped.transform.rotation.x;
+	  p.pose.orientation.y = transformStamped.transform.rotation.y;
+	  p.pose.orientation.z = transformStamped.transform.rotation.z;
+	  p.pose.orientation.w = transformStamped.transform.rotation.w;
+	  pose_pub.publish(p);
+	  
+	  ROS_DEBUG_THROTTLE(0.5, "time: %d -- time_previous: %d : (x,y,z) = (%f, %f, %f)", timestampprev, timestamp, objPose[0].Pos[0], objPose[0].Pos[1], objPose[0].Pos[2]);
         }
       timestampprev = timestamp;
     }
